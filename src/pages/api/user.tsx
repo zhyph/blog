79433ce12401a1548cec0be1dccf8c27cc7ac218
Artur Almeida <artuzinhotutu00@hotmail.com>
2021-05-31 20:48:34 -0300

    geting collection

--- src/pages/api/user.tsx
+++ src/pages/api/user.tsx
@@ -9,7 +9,7 @@ interface ResponseType {
 
 export default async (
   req: NextApiRequest,
-  res: NextApiResponse<ResponseType>
+  res: NextApiResponse
 ): Promise<void> => {
   const { db } = await connect();
 
@@ -19,6 +19,17 @@ export default async (
       age: 22,
     });
     res.status(200).json(response.ops[0]);
+  } else if (req.method === 'GET') {
+    const response = await db
+      .collection('users')
+      .find({ name: { $exists: true } })
+      .toArray()
+      .then((items) => {
+        console.log(`Successfully found ${items.length} documents`);
+        items.forEach(console.log);
+        return items;
+      });
+    res.status(200).json(response);
   } else {
     res.status(400).json({ message: 'Wrong Method' });
   }
