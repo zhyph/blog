5d7005bd80c93d83a2acdd776a3605cf94eb5be3
Artur Almeida <artuzinhotutu00@hotmail.com>
2021-05-31 21:43:14 -0300

    Create /use api route

--- src/pages/api/user.tsx
+++ src/pages/api/user.tsx
@@ -3,23 +3,37 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import connect from '../../utils/database';
 
-interface ResponseType {
-  message: string;
+interface ErrorResponseType {
+  error: string;
+}
+
+interface SuccesResponseType {
+  _id: string;
+  name: string;
+  email: string;
+  image: string;
 }
 
 export default async (
   req: NextApiRequest,
-  res: NextApiResponse<ResponseType>
+  res: NextApiResponse<ErrorResponseType | SuccesResponseType>
 ): Promise<void> => {
-  const { db } = await connect();
-
   if (req.method === 'POST') {
+    const { name, email, image } = req.body;
+
+    if (!name || !email || !image) {
+      res.status(400).json({ error: 'Missing Body Parameters' });
+      return;
+    }
+    const { db } = await connect();
+
     const response = await db.collection('users').insertOne({
-      name: 'Daniel',
-      age: 22,
+      name,
+      email,
+      image,
     });
     res.status(200).json(response.ops[0]);
   } else {
-    res.status(400).json({ message: 'Wrong Method' });
+    res.status(400).json({ error: 'Wrong Method' });
   }
 };
