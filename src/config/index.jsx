c8d3b65154518cf74ba7de0235fc1902b3b5c41e
Artur Almeida <artuzinhotutu00@hotmail.com>
2021-06-05 16:59:46 -0300

    newerror

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://blog-zhyph.vercel.app';
+  : 'https://blog-zhyph.vercel.app/';
