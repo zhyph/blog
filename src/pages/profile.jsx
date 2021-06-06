0dbc1195e6ee337dcb19af181b4afaaee2141a87
Artur Almeida Junior <68618179+zhyph@users.noreply.github.com>
2021-06-06 12:30:47 -0300

    Update profile.jsx

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -77,11 +77,6 @@ const useStyles = makeStyles((theme) => ({
     placeItems: "center",
   },
   loggedInContainer: {
-    // display: 'grid',
-    // placeItems: 'center',
-    // height: '70%',
-    // paddingTop: '1rem',
-    // marginBottom: theme.spacing(1),
     [theme.breakpoints.down("md")]: {
       maxHeight: "660px",
       maxWidth: "960px",
@@ -135,19 +130,14 @@ const useStyles = makeStyles((theme) => ({
       flex: "1 1 80px",
       // backgroundColor: 'purple',
     },
-    // ' & #grid': {
-    //   display: 'grid',
-    //   placeItems: 'center',
-    // },
+    
   },
   root: {
     width: "100%",
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
-    // display: 'grid',
-    // placeItems: 'center',
-    // gridTemplateColumns: 'repeat(5, 1fr)',
+ 
   },
   textSize: {
     [theme.breakpoints.up("md")]: {
@@ -156,10 +146,7 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down("md")]: {
       fontSize: "1rem",
     },
-    // [theme.breakpoints.down('sm')]: {
-    //   fontSize: '.25rem',
-    //   // backgroundColor: 'purple',
-    // },
+  
   },
   buttonsWrapper: {
     display: "flex",
@@ -370,11 +357,7 @@ const profile = ({ dados }) => {
         if (data && !data.error && data.message) {
           setPatchSucces(data.message);
         }
-        // if (data && data.token) {
-        //   //Set cookie
-        //   cookie.set('token', data.token, { expires: 2 });
-        //   Router.push('/');
-        // }
+        
       });
     ref.current.value = "";
     setFormData({
@@ -410,11 +393,7 @@ const profile = ({ dados }) => {
         if (data && !data.error && data.message) {
           setPatchSucces(data.message);
         }
-        // if (data && data.token) {
-        //   //Set cookie
-        //   cookie.set('token', data.token, { expires: 2 });
-        //   Router.push('/');
-        // }
+       
       });
     ref.current.value = "";
     setFormData({
@@ -427,24 +406,7 @@ const profile = ({ dados }) => {
     });
   };
 
-  // useEffect(() => {
-  //   console.log('testeputa');
-  //   setNewTest(finalBase64Src());
-  // }, []);
-
-  // console.log(userIdCheck());
-
-  // console.log(ref?.current?.value);
-
-  // const handleChange = (event) => {
-  //   setValues({
-  //     ...values,
-  //     [event.target.name]: event.target.value,
-  //   });
-  // };
-
-  // console.log(formData);
-  // console.log(newTest);
+ 
 
   return (
     <Box height={"100vh"} width="auto" className={classes.body}>
@@ -492,320 +454,3 @@ export const getServerSideProps = async (ctx) => {
     },
   };
 };
-
-// <Container className={classes.loggedInBox}>
-//   <Box className={classes.loggedFormBox}>
-//     <Typography variant="h6" className={classes.textSize}>
-//       Preencha o formulario se você desejar editar ou excluir algum
-//       usuario
-//     </Typography>
-//     <form
-//       className={classes.root}
-//       onSubmit={(e) => handleSubmit(e)}
-//       autoComplete="off"
-//     >
-//       <FormControl
-//         component="fieldset"
-//         className={classes.inputComponent}
-//       >
-//         <TextField
-//           label="UserID"
-//           variant="filled"
-//           inputRef={ref}
-//           required
-//           error={errorText.userId}
-//           helperText={errorText.textUserId}
-//           // value={formData.userId}
-//           // onChange={(e) => setFormData({ userId: e.target.value })}
-//           onBlur={(e) => userIdCheck(e)}
-//         />
-//       </FormControl>
-//       <FormControl
-//         component="fieldset"
-//         className={classes.inputComponent}
-//       >
-//         <TextField
-//           label="Nome"
-//           variant="filled"
-//           value={formData.name}
-//           required
-//           onChange={(e) =>
-//             setFormData((prevState) => ({
-//               ...prevState,
-//               name: e.target.value,
-//             }))
-//           }
-//         />
-//       </FormControl>
-//       <FormControl
-//         component="fieldset"
-//         className={classes.inputComponent}
-//       >
-//         <TextField
-//           label="Email"
-//           type="email"
-//           required
-//           variant="filled"
-//           value={formData.email}
-//           onChange={(e) =>
-//             setFormData((prevState) => ({
-//               ...prevState,
-//               email: e.target.value,
-//             }))
-//           }
-//         />
-//       </FormControl>
-//       <FormControl
-//         component="fieldset"
-//         className={classes.inputComponent}
-//       >
-//         <TextField
-//           label="CPF"
-//           variant="filled"
-//           InputProps={{
-//             inputComponent: TextMaskCustom,
-//             value: formData.cpf,
-//             // value: this.state.textmask,
-//             // onChange: this.handleChange('textmask'),
-//             onBlur: (e) => errorCheck(e.target.value),
-//             onChange: (e) =>
-//               setFormData((prevState) => ({
-//                 ...prevState,
-//                 cpf: e.target.value,
-//               })),
-//           }}
-//           required
-//           error={errorText.cpf}
-//           helperText={errorText.textCpf}
-//           // }
-//         />
-//       </FormControl>
-//       <FormControl
-//         component="fieldset"
-//         className={classes.inputComponent}
-//       >
-//         <InputLabel id="demo-simple-select-label">Nivel</InputLabel>
-//         <Select
-//           labelId="demo-simple-select-label"
-//           id="demo-simple-select"
-//           value={formData.type}
-//           onChange={(e) =>
-//             setFormData((prevState) => ({
-//               ...prevState,
-//               type: e.target.value,
-//             }))
-//           }
-//         >
-//           <MenuItem value={'1'}>Usuario Comum</MenuItem>
-//           <MenuItem value={'999'}>Admin</MenuItem>
-//         </Select>
-//       </FormControl>
-//       <div className={classes.teste}>
-//         <FormControl
-//           component="fieldset"
-//           className={classes.inputComponent}
-//           // id="grid"
-//         >
-//           <RadioGroup
-//             row
-//             // className={classes.radioButton}
-//             aria-label="position"
-//             name="position"
-//             value={formData.active}
-//             onChange={(e) =>
-//               setFormData((prevState) => ({
-//                 ...prevState,
-//                 active: e.target.value,
-//               }))
-//             }
-//           >
-//             <FormControlLabel
-//               value="1"
-//               control={<Radio color="primary" />}
-//               label="Ativado"
-//               labelPlacement="start"
-//             />
-//             <FormControlLabel
-//               value="0"
-//               control={<Radio color="primary" />}
-//               label="Desativado"
-//               labelPlacement="start"
-//             />
-//           </RadioGroup>
-//         </FormControl>
-//         <div className={classes.buttonsWrapper}>
-//           <Button
-//             type="submit"
-//             variant="contained"
-//             className={classes.buttonError}
-//             onClick={(e) => handleDelete(e)}
-//           >
-//             Deletar
-//           </Button>
-//           <Button variant="contained" onClick={(e) => handleCancel(e)}>
-//             Cancelar
-//           </Button>
-//           <Button
-//             type="submit"
-//             variant="contained"
-//             color="primary"
-//             // disabled={formData.userId ? false : true}
-//           >
-//             Enviar
-//           </Button>
-//         </div>
-//       </div>
-//     </form>
-//     {patchSuccess && (
-//       <Alert className={classes.alertMargin} severity="success">
-//         {patchSuccess}
-//       </Alert>
-//     )}
-//     {patchError && (
-//       <Alert className={classes.alertMargin} severity="error">
-//         {patchError}
-//       </Alert>
-//     )}
-//   </Box>
-//   <Box className={classes.loggedInContainer}>
-//     <TableContainer component={Paper}>
-//       <Table
-//         className={classes.table}
-//         size="small"
-//         aria-label="a dense table"
-//       >
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>UserID</TableCell>
-//             <TableCell align="right">Nome</TableCell>
-//             <TableCell align="right">Email</TableCell>
-//             <TableCell align="right">CPF</TableCell>
-//             <TableCell align="right">Nivel de Acesso</TableCell>
-//             <TableCell align="right">Ativo</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {dados?.map((row) => (
-//             <TableRow key={row.userId}>
-//               <TableCell component="th" scope="row">
-//                 {row.userId}
-//               </TableCell>
-//               <TableCell align="right">{row.name}</TableCell>
-//               <TableCell align="right">{row.email}</TableCell>
-//               <TableCell align="right">{row.cpf}</TableCell>
-//               <TableCell align="right">{row.type}</TableCell>
-//               <TableCell align="right">
-//                 {row.active === '1' ? 'Ativo' : 'Desativado'}
-//               </TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   </Box>
-// </Container>
-// <Container className={classes.normalUserLoggedInBox}>
-//   <Box className={classes.normalUserBox}>
-//     <Box className={classes.newBoxTest} spacing={3}>
-//       <Box
-//         // item xs={6}
-//         className={classes.normalUserElement}
-//       >
-//         <img
-//           width="60%"
-//           className={classes.profilePicture}
-//           // layout="responsive"
-//           src={
-//             finalBase64Src() ? finalBase64Src().base64 : '/default.png'
-//           }
-//         />
-//       </Box>
-//       <Box
-//         // item xs={6}
-//         className={classes.normalUserElement}
-//       >
-//         <form
-//           className={classes.root}
-//           onSubmit={(e) => handleSubmit(e)}
-//           autoComplete="off"
-//         >
-//           <FormControl
-//             component="fieldset"
-//             className={classes.inputComponent}
-//           >
-//             <TextField
-//               label="Nome"
-//               value={formData.name}
-//               required
-//               onChange={(e) =>
-//                 setFormData((prevState) => ({
-//                   ...prevState,
-//                   name: e.target.value,
-//                 }))
-//               }
-//             />
-//           </FormControl>
-//           <FormControl
-//             component="fieldset"
-//             className={classes.inputComponent}
-//           >
-//             <TextField
-//               label="Email"
-//               type="email"
-//               required
-//               // value={newTest.email}
-//               onChange={(e) =>
-//                 setNewTest((prevState) => ({
-//                   ...prevState,
-//                   email: e.target.value,
-//                 }))
-//               }
-//             />
-//           </FormControl>
-//           <FormControl
-//             component="fieldset"
-//             className={classes.inputComponent}
-//           >
-//             <TextField
-//               label="CPF"
-//               InputProps={{
-//                 inputComponent: TextMaskCustom,
-//                 value: formData.cpf,
-//                 // value: this.state.textmask,
-//                 // onChange: this.handleChange('textmask'),
-//                 onBlur: (e) => errorCheck(e.target.value),
-//                 onChange: (e) =>
-//                   setFormData((prevState) => ({
-//                     ...prevState,
-//                     cpf: e.target.value,
-//                   })),
-//               }}
-//               required
-//               error={errorText.cpf}
-//               helperText={errorText.textCpf}
-//               // }
-//             />
-//           </FormControl>
-//           <div className={classes.teste}>
-//             <div className={classes.buttonsWrapper}>
-//               <Button
-//                 variant="contained"
-//                 onClick={(e) => handleCancel(e)}
-//               >
-//                 Cancelar
-//               </Button>
-//               <Button
-//                 type="submit"
-//                 variant="contained"
-//                 color="primary"
-//                 // disabled={formData.userId ? false : true}
-//               >
-//                 Enviar
-//               </Button>
-//             </div>
-//           </div>
-//         </form>
-//       </Box>
-//     </Box>
-//   </Box>
-// </Container>
